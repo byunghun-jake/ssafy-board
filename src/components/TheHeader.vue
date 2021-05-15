c30d48e387fa14a93a647476ee6c7540e60a649a
김병훈 <figma@kakao.com>
2021-05-15 21:04:30 +0900

    update header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,9 +4,9 @@
       <div class="flex">
         <span class="text-white font-bold text-2xl">스승의 날 축전</span>
       </div>
-      <span class="text-white font-bold text-2xl"
-        >김구현 교수님 감사합니다!</span
-      >
+      <span class="text-white font-bold text-2xl">
+        교수님 감사합니다!
+      </span>
     </div>
   </nav>
 </template>
